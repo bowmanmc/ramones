976933a6aecee5262cd4e170ed4cd0e252248a96
Michael Bowman <bowmanmc@gmail.com>
2018-10-27 21:45:09 -0400

    Song lengths bar chart with vega

---
+++ src/components/charts/Colors.js
@@ -0,0 +1,11 @@
+
+export default {
+    GRAY_95: '#ededed',
+    GRAY_90: '#e3e3e3',
+    GRAY_50: '#808080',
+    GRAY_40: '#696969',
+    GRAY_30: '#4d4d4d',
+    GRAY_20: '#363636',
+    GRAY_TRANSPARENT_09: 'rgba(0, 0, 0, 0.09)',
+    GRAY_TRANSPARENT_25: 'rgba(0, 0, 0, 0.25)',
+};
--- src/components/charts/SongLengths.vue
+++ src/components/charts/SongLengths.vue
@@ -8,6 +8,8 @@
 <script>
 import vegaEmbed from 'vega-embed';
 
+import SongLengthsSpec from './SongLengthsSpec';
+
 
 export default {
     name: 'SongLengths',
@@ -23,52 +25,10 @@ export default {
                 return;
             }
 
-            const spec = {
-                $schema: 'https://vega.github.io/schema/vega-lite/v3.json',
-                description: 'Bar chart showing the lengths of songs off of the first Ramones album.',
-                width: 768,
-                height: 480,
-                config: {
-                    view: {
-                        stroke: 'transparent',
-                    },
-                },
-                autosize: {
-                    type: 'fit',
-                    contains: 'padding',
-                },
-                data: {
-                    values: this.songs,
-                },
-                mark: {
-                    type: 'bar',
-                    color: '#363636',
-                    tooltip: '',
-                },
-                encoding: {
-                    x: {
-                        field: 'Seconds',
-                        type: 'quantitative',
-                        axis: {
-                            title: 'Song Length (seconds)',
-                            grid: true,
-                            gridColor: 'rgba(0, 0, 0, 0.09)',
-                        },
-                    },
-                    y: {
-                        field: 'Title',
-                        sort: null,
-                        type: 'nominal',
-                        scale: {
-                            paddingInner: 0.35,
-                        },
-                        axis: {
-                            title: '',
-                            grid: false,
-                        },
-                    },
-                },
-            };
+            const spec = Object.assign({}, SongLengthsSpec, {
+                data: { values: this.songs },
+            });
+
             vegaEmbed(this.$refs.chart, spec, {
                 actions: false,
                 renderer: 'svg',
@@ -85,22 +45,6 @@ export default {
     @import "../../defaults/colors";
 
     .SongLengths {
-
-        .chart-bar-horizontal {
-            width: 100%;
-
-            .marks path {
-                cursor: default;
-            }
-        }
-
-        .ct-series-a .ct-bar {
-            stroke: $color-gray-20;
-            stroke-width: 17px;
-
-            &:hover {
-                stroke: $color-gray-30;
-            }
-        }
+        cursor: default;
     }
 </style>
